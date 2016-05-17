0570b86c983e80fbe88c61dea1d7961572e03d69
nico <nicolas.angel.ledesma@gmail.com>
2016-05-17 01:25:20 -0300

    actualizacion del tp al estado ultimo. agregado de la parte de generar sorteo corregida

---
+++ Grupo09/Grupo09/resguardo/Instalador.sh
@@ -0,0 +1,22 @@
+tar -xvzf Grupo09.tgz
+cd ./Grupo09/config/
+if [[ -f CIPAK.cnf ]]; then #si no esta instalado esto da false 
+    echo Ya esta creado
+else
+    echo "GRUPO=/home/$USER/Grupo09=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+CONFDIR=/home/$USER/Grupo09/config=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+BINDIR=/home/$USER/Grupo09/binarios=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+MAEDIR=/home/$USER/Grupo09/maestros=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+ARRIDIR=/home/$USER/Grupo09/arribados=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+OKDIR=/home/$USER/Grupo09/aceptados=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+PROCDIR=/home/$USER/Grupo09/procesados=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+INFODIR=/home/$USER/Grupo09/informes=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+LOGDIR=/home/$USER/Grupo09/bitacoras=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+NOKDIR=/home/$USER/Grupo09/rechazados=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+BACKUP=/home/$USER/Grupo09/resguardo=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+LOGSIZE=22=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+SLEEPTIME=20=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+IDSORTEO=1=$USER=$(date '+%Y-%m-%d %H:%M:%S')
+" > CIPAK.cnf
+fi
+cd ..
---
+++ Otros/Mover.sh
@@ -0,0 +1,3 @@
+if [ 1 -lt 2 ]; then
+    echo 'pepe'
+fi
